fa5be00b97e27d58257d7e1a0efed1edcfd91609
yemingrujing <yemingrujing@users.noreply.github.com>
2019-08-13 21:47:06 +0800

    建立博客应用

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页!")
+    post_list = Post.objects.all().order_by('-create_time')
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list
+    })
