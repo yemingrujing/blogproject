e5fd6b8fd583fcc8d963f1e26a93623dcf317dbb
yemingrujing <yemingrujing@users.noreply.github.com>
2019-08-13 16:07:40 +0800

    建立博客应用

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    '''
+    Django 要求模型必须继承 models.Model 类。
+    Category 只需要一个简单的分类名 name 就可以了。
+    CharField 指定了分类名 name 的数据类型，CharField 是字符型，
+    CharField 的 max_length 参数指定其最大长度，超过这个长度的分类名就不能被存入数据库。
+    当然 Django 还为我们提供了多种其它的数据类型，如日期时间类型 DateTimeField、整数类型 IntegerField 等等。
+    Django 内置的全部类型可查看文档：
+    '''
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    # 标签 Tag
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    # 文章标题
+    title = models.CharField(max_length=70)
