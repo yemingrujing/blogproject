d706a9c3ece2f7a2ee1cf91209a50ebe653b75c2
Wei.Guang <yemingrujing@163.com>
2019-08-20 00:09:54 +0800

    上传与下载

--- upload/views.py
+++ upload/views.py
@@ -6,6 +6,14 @@ from django.http import StreamingHttpResponse
 from blogproject import settings
 import os, uuid, hashlib
 
+from io import StringIO
+from io import open
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from docx import Document
+
 
 # Create your views here.
 class Uploads(View):
@@ -14,7 +22,7 @@ class Uploads(View):
 
     def post(self, request):
         my_file = request.FILES.get('file_name', )
-        file_name = get_unique_str() + '.' + my_file.name.split('.')[-1]
+        file_name = get_unique_str() + '_' + my_file.name
         # 文件路径
         file_path = os.path.join(settings.UPLOAD_ROOT, file_name)
         f = open(file_path, 'wb')
@@ -73,3 +81,37 @@ def read_file(file_name, chunk_size=512):
                 yield c
             else:
                 break
+
+
+def read_from_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        resource_manager = PDFResourceManager()
+        return_str = StringIO()
+        lap_params = LAParams()
+
+        device = TextConverter(
+            resource_manager, return_str, laparams=lap_params)
+        process_pdf(resource_manager, device, file)
+        device.close()
+
+        content = return_str.getvalue()
+        return_str.close()
+        return content
+
+
+def save_text_to_word(content, file_path):
+    doc = Document()
+    for line in content.split('\n'):
+        paragraph = doc.add_paragraph()
+        paragraph.add_run(remove_control_characters(line))
+    doc.save(file_path)
+
+
+def remove_control_characters(content):
+    mpa = dict.fromkeys(range(32))
+    return content.translate(mpa)
+
+
+def pdf_to_word(pdf_file_path, word_file_path):
+    content = read_from_pdf(pdf_file_path)
+    save_text_to_word(content, word_file_path)
