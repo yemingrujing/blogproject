d06f841ec4ee4c47c35d7e022168e7bb768e7490
yemingrujing <yemingrujing@users.noreply.github.com>
2019-09-03 10:06:25 +0800

    服务器访问

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'z5*cesbqf_5lcf)1^vpqg*z@t=d5&g^lete)2@!^t8%zpzwmo@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
